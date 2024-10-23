fa9e9db5b796fb2b617c3e13946fae95b1b9814d
10154 <matteogirotti10@gmail.com>
2024-10-23 10:51:30 +0200

    commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,j,na,n,k;
+    printf("quante righe vuoi? ");
+    scanf("%d",&n);
+    n>0;
+    for(i=0;i<n;i++){
+        for(j=0;j<i+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+        printf("quante righe vuoi? ");
+        scanf("%d", &n);
+    (n>0);
+    for(i=0;i<n;i++){
+        for(j=n;j>i+1;j--){
+            for(i=0;i<k;i++);
+            printf(" ");
+            printf(" *");
+
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
